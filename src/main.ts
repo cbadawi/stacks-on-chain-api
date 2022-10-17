894ca8e1a712d790e1224c23f67db24395f29f8d
cbadawi <cb_1974@protonmail.com>
2022-10-17 18:00:08 +0300

    chore : use cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: '*',
+  maxAge: 300,
+  methods: 'GET,HEAD,POST',
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: corsOptions });
   await app.listen(3000);
 }
 bootstrap();
