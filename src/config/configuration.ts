372848a26f0dbb5dd228e36025ca47c81fe11b1b
cbadawi <cb_1974@protonmail.com>
2022-10-30 11:46:34 +0700

    add mongo configs

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,7 @@
 export default () => ({
   PORT: parseInt(process.env.PORT, 10) || 80,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'localhost',
   DATABASE_URL: process.env.DATABASE_URL,
+  NODE_BASE_URL:
+    process.env.NODE_BASE_URL || 'https://stacks-node-api.mainnet.stacks.co', // stacks mainnet throttles api calls, better to have your own node running.
 });
